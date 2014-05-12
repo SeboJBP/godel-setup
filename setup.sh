5f2c19272fb8abdaabd7577d39ecb00ec0c6a3e0
Sebastian Jimenez <sebastian.jbp@gmail.com>
2014-05-12 06:55:15 +0000

    added google appengine setup

--- setup.sh
+++ setup.sh
@@ -2,13 +2,24 @@
 # A simple sh script to configuring fresh Ubuntu 14.04 LTS EC2 instance
 # Seamless process to get my dev env up and running
 
-# Install basic tools
+# Make $HOME/bin folder
+mkdir -p $HOME/bin
+
+# Install basic utils/tools
 sudo apt-get update
 sudo apt-get install -y curl	# curl util
 sudo apt-get install -y make gcc # make util and C compiler
+sudo apt-get install -y unzip
+sudo apt-get install -y python-pip # install pip
 
 # Install latest emacs
 sudo apt-get install -y emacs
+
+# TODO (SeboJBP): Automatize Python set up for my needs.
+
+# Install Google App Engine
+wget -P $HOME/bin/ https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.4.zip
+unzip $HOME/bin/google_appengine_1.9.4.zip
 
 # git pull and install dotfiles as well
 cd $HOME
@@ -26,5 +37,3 @@ ln -sb godel-dotfiles/.bash_aliases .
 ln -sb godel-dotfiles/.gitconfig .
 ln -sb godel-dotfiles/.screenrc .
 ln -sf godel-dotfiles/.emacs.d .
-
-# TODO (SeboJBP): Automatize Python set up for my needs.
