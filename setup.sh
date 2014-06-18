26c482ca89c1ed19ec46025e0c00363d1fe356df
Sebastian Jimenez <sebastian.jbp@gmail.com>
2014-06-18 05:23:36 +0000

    Added Python scikit-learn installation

--- setup.sh
+++ setup.sh
@@ -17,8 +17,11 @@ sudo apt-get install -y emacs
 
 # TODO (SeboJBP): Automate Python library installation 
 # Install Python data-related libraries ("SciPy Stack")
-# ref: http://www.scipy.org/stackspec.html
+# see: http://www.scipy.org/stackspec.html
 sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+# Install python scikit-learn package 
+# see: http://scikit-learn.org/
+sudo apt-get install -y python-sklearn
 
 # Install Google App Engine
 wget -P $HOME/bin/ https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.4.zip
